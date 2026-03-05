77d2dfc74874c52facbe0cd0cfb34eb8ca884348
Morgan Powers <morgan.c.powers@gmail.com>
2026-03-05 11:54:39 -0600

    add form fixed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const router = express.Router();
 app.set('view engine','ejs');
 const port = 9001;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:false}));
 
 const connection = require('./database');
 
@@ -35,10 +36,17 @@ app.get('/recipe/:recipeID', (req,res) => {
     });
 });
 
-app.get('/addRecipe', (req, res) => { 
-    res.render('addRecipe', function (err,html) {
-        if (err) console.error(err);
-        res.send(html);
+app.get('/addRecipe', (req, res) => {
+    connection.query('SELECT DISTINCT Ingredient FROM ingredients;', (err,result) => {
+        const ingredients = result;
+        if(err) console.error(err);
+        connection.query('SELECT DISTINCT Protein FROM recipes;', (err,result) => {
+            if(err) console.error(err);
+            res.render('addRecipe', {ingredients,proteins: result}, function (err,html) {
+                if (err) console.error(err);
+                res.send(html);
+            });
+        })
     });
 });
 
@@ -47,28 +55,41 @@ app.get('/recipeList', (req, res) => {
 
     connection.query(sql, (err, result) => {
         if(err) throw err;
-        console.log(result);
-        res.render('recipeList', {recipes: result}, function (err,html) {
+        //console.log(result);
+        const beef = result.filter(r => r.Protein === 'Beef');
+        const chicken = result.filter(r => r.Protein === 'Chicken');
+        res.render('recipeList', {chicken, beef}, function (err,html) {
             if (err) console.error(err);
             res.send(html);
         });
     });
 });
 
-router.post("/add_recipe", function (req, res) {
-    //make next IDs
-    const mr = db.query('SELECT MAX(RecipeID) AS maxID FROM recipes');
-    const id = (mr.rows ? mr.rows[0].max_value : mr[0].max_value) + 1;
-
-    //Should ingredient insertion be here or in addRecipe?
-	let sql = `
-        INSERT INTO recipes(RecipeName, RecipeID) VALUES (?, ${id});
-        INSERT INTO ingredients(RecipeID, Ingredient, Description) VALUES (${id}, ?, ?);
-    `;
-	console.log(req.body);
-    db.query(sql,[req.body.title, req.body.ingredients], (err, result) => {
-        if(err) throw err;
-        res.redirect('../db');
+app.post("/add_recipe", function (req, res) {
+    console.log(req.body);
+    connection.query('SELECT MAX(RecipeID) AS maxID FROM recipes', (err,result) => {
+        if(err) {
+            console.log("Error on recipe insert");
+            throw err;
+        } const id = result[0].maxID+1;
+        connection.query(`INSERT INTO recipes(RecipeName, RecipeID, Protein) VALUES (?,?,?);`,[req.body.title, id, req.body.protein], (err, _) => {
+            if(err) {
+                console.log("Error on recipe insert");
+                throw err;
+            } req.body.ingredients.forEach(i => {
+                connection.query(`SELECT DISTINCT Description FROM ingredients WHERE Ingredient = ?;`,i, (err,result) => {
+                    if(err) {
+                        console.log("Error on ingredient desc get");
+                        throw err;
+                    } connection.query(`INSERT INTO ingredients(RecipeID, Ingredient, Description) VALUES (?, ?, ?);`,[id,i,result[0].Description], (err, _) => {
+                        if(err) {
+                            console.log("Error on ingredient insert");
+                            throw err;
+                        }
+                    });
+                });
+            });
+        });
     });
+    res.redirect('/recipeList');
 });
-
